b4d92c45e4cf3e1e83decef8d2f0c102e5daf9a0
ca-is <165729256+ca-is@users.noreply.github.com>
2024-11-04 18:52:28 +0100

    spideroak ready

--- js/nav4.js
+++ js/nav4.js
@@ -1,7 +1,9 @@
 function openNav() {
   document.getElementById("navOverlay").classList.add("open");
+  $('#footer').css('display', 'none');
 }
 
 function closeNav() {
   document.getElementById("navOverlay").classList.remove("open");
+  $('#footer').css('display', 'block');
 }
